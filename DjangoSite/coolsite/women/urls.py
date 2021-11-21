85fa0da202679453d5974066c3b78e4913f6c2bb
sbezr <sbezroukov@gmail.com>
2021-11-21 22:07:59 +0300

    test

--- DjangoSite/coolsite/women/urls.py
+++ DjangoSite/coolsite/women/urls.py
@@ -5,11 +5,12 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),  # http://127.0.0.1:8000/
     path('about/', about, name='about'),
-    path('addpage/', about, name='add_page'),
-    path('contract/', about, name='contract'),
-    path('login/', about, name='login'),
-    path('cats/', categories),  # http://127.0.0.1:8000/cats2/
-    path('cats2/<int:catid>/', categories2),  # http://127.0.0.1:8000/cats2/1/
-    path('cats2/<slug:cat>/', categories3),  # http://127.0.0.1:8000/cats2/1/
-    re_path(r'archive/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})', archive),  # http://127.0.0.1:8000/cats2/1/
+    path('addpage/', add_page, name='add_page'),
+    path('contract/', contract, name='contract'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post')
+    # path('cats/', categories),  # http://127.0.0.1:8000/cats2/
+    # path('cats2/<int:catid>/', categories2),  # http://127.0.0.1:8000/cats2/1/
+    # path('cats2/<slug:cat>/', categories3),  # http://127.0.0.1:8000/cats2/1/
+    # re_path(r'archive/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})', archive),  # http://127.0.0.1:8000/cats2/1/
 ]
--- DjangoSite/coolsite/women/views.py
+++ DjangoSite/coolsite/women/views.py
@@ -6,7 +6,7 @@ from .models import Women
 
 menu = [{'title': "О сайте", 'url_name': "about"},
         {'title': "Добавить статью", 'url_name': "add_page"},
-        {'title': "Обратная связь", 'url_name': "contact"},
+        {'title': "Обратная связь", 'url_name': "contract"},
         {'title': "Войти", 'url_name': "login"}]
 
 
@@ -16,7 +16,7 @@ def index(request):  # HttpRequest
     context = {
         'posts': posts,
         'menu': menu,
-        'title': 'Главная страница 2'
+        'title': 'Главная страница'
     }
     return render(request, 'women/index.html',  context=context)
 
@@ -27,6 +27,7 @@ def about(request):
 
 
 def add_page(request):
+
     return HttpResponse('Добавление статьи')
 
 
@@ -38,34 +39,38 @@ def login(request):
     return HttpResponse('Авторизация')
 
 
-
-
-
-def categories(request):
-    return HttpResponse("<h1>Статьи по категорям</h1>")
-
-
-def categories2(request, catid):
-    if request.GET:
-        print(request.GET)
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категорям</h1><p>{catid}</p>")
-
-
-def categories3(request, cat):
-    return HttpResponse(f"<h1>Статьи по категорям</h1><p>{cat}</p>")
-
-
-def archive(request, year, month):
-    if int(year) > 2020:
-        raise Http404()
-    if int(month) > 20:
-        #return redirect('/', permanent=True)
-        return redirect('home', permanent=True)
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}-{month}</p>")
-
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound(f'<h1>Страница не найдена</h1> {exception}')
+
+
+def show_post(request, post_id):
+    return HttpResponseNotFound(f'Отображение статьи с id = {post_id}')
+
+#
+#
+# def categories(request):
+#     return HttpResponse("<h1>Статьи по категорям</h1>")
+#
+#
+# def categories2(request, catid):
+#     if request.GET:
+#         print(request.GET)
+#     if request.POST:
+#         print(request.POST)
+#     return HttpResponse(f"<h1>Статьи по категорям</h1><p>{catid}</p>")
+#
+#
+# def categories3(request, cat):
+#     return HttpResponse(f"<h1>Статьи по категорям</h1><p>{cat}</p>")
+#
+#
+# def archive(request, year, month):
+#     if int(year) > 2020:
+#         raise Http404()
+#     if int(month) > 20:
+#         #return redirect('/', permanent=True)
+#         return redirect('home', permanent=True)
+#
+#     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}-{month}</p>")
+
+
